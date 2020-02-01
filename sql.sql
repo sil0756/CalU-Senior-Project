e05fdb6785edd539a6b31b1433ee75feb3654303
Ryn0113 <40875234+Ryn0113@users.noreply.github.com>
2020-02-01 14:53:25 -0500

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,92 +1,114 @@
 
-DROP DATABASE 
-IF EXISTS bronco;
-
-DROP TABLE 
-IF EXISTS bronco.phonebook;
-
-DROP TABLE 
-IF EXISTS bronco.inventory;
-
-DROP TABLE 
-IF EXISTS bronco.projects;
-
-DROP TABLE 
-IF EXISTS bronco.workcompleted;
-
+DROP DATABASE IF EXISTS bronco;
 CREATE DATABASE bronco;
 USE bronco; 
   
-                 CREATE TABLE phonebook 
-                 ( 
-                              contactid     INT auto_increment, 
-                              firstname     VARCHAR(60), 
-                              lastname      VARCHAR(60), 
-                              businessname  VARCHAR(60), 
-                              addr1         VARCHAR(60), 
-                              addr2         VARCHAR(60) DEFAULT NULL, 
-                              city          VARCHAR(40), 
-                              state         VARCHAR(2), 
-                              zipcode       VARCHAR(10), 
-                              emailaddress  VARCHAR(255), 
-                              mobilephone   VARCHAR(12), 
-                              homephone     VARCHAR(12), 
-                              businessphone VARCHAR(12), 
-                              PRIMARY KEY (contactid), 
-                              UNIQUE INDEX emailaddress (emailaddress) 
-                 );
+CREATE TABLE phonebook 
+( 
+    phoneid      INT             NOT NULL    auto_increment, 
+    firstname    VARCHAR(60)     NOT NULL, 
+    lastname     VARCHAR(60)     NOT NULL, 
+    business     VARCHAR(60)     NOT NULL, 
+    addr1        VARCHAR(60)     NOT NULL, 
+    addr2        VARCHAR(60)                 DEFAULT NULL, 
+    city         VARCHAR(40)     NOT NULL, 
+    state        VARCHAR(2)      NOT NULL, 
+    zip          VARCHAR(10)     NOT NULL, 
+    emailaddress VARCHAR(255)    NOT NULL, 
+    phonenumber  VARCHAR(12)     NOT NULL, 
+    PRIMARY KEY (phoneid), 
+    UNIQUE INDEX emailaddress (emailaddress) 
+);
+
+CREATE TABLE parts 
+( 
+    partid      INT             NOT NULL    auto_increment, 
+    itemname    VARCHAR(60)     NOT NULL, 
+    itemdesc    VARCHAR(255)    NOT NULL, 
+    quantity    INT             NOT NULL, 
+    PRIMARY KEY (partid) 
+);
+
+CREATE TABLE transaction
+(
+    transid      INT             NOT NULL    auto_increment,
+    phoneid      INT             NOT NULL,
+    partid       INT             NOT NULL,
+    date         DATETIME        NOT NULL,
+    price        DECIMAL(10,2)   NOT NULL,
+    transtype    VARCHAR(10)     NOT NULL,
+    quantity     INT             NOT NULL,
+    PRIMARY KEY (transid),
+    INDEX phoneid (phoneid),
+    INDEX partid (partid)
+    /*FOREIGN KEY (phoneid) REFERENCES phonebook (phoneid),
+    FOREIGN KEY (partid) REFERENCES parts (partid)*/
+);
+
+CREATE TABLE projects 
+( 
+    projectid       INT           NOT NULL    auto_increment, 
+    projectname     VARCHAR(60)   NOT NULL, 
+    make            VARCHAR(60)   NOT NULL,
+    model           VARCHAR(60)   NOT NULL,
+    trim_pkg        VARCHAR(60)   NOT NULL       
+    projectdesc     VARCHAR(255)  NOT NULL, 
+    purchprice      DECIMAL(10,2) NOT NULL, 
+    purchdate       DATETIME      NOT NULL, 
+    sellprice       DECIMAL(10,2) NOT NULL, 
+    selldate        DATETIME,     NOT NULL
+    projectcomments VARCHAR(255),
+    PRIMARY KEY (projectid), 
+    UNIQUE INDEX (projectname),
+    INDEX projectid (projectid)
+);
+
+CREATE TABLE project_parts
+(
+    partid      INT             NOT NULL,
+    projectid   INT             NOT NULL,
+    PRIMARY KEY (partid, projectid),
+    INDEX partid (partid),
+    INDEX projectid (projectid)
+    /*FOREIGN KEY (partid) REFERENCES parts (partid),
+    FOREIGN KEY (projectid) REFERENCES projects (projectid)*/  
+);
                  
-                 CREATE TABLE inventory 
-                 ( 
-                              invid      INT auto_increment, 
-                              projectid  INT, 
-                              itemname   VARCHAR(60), 
-                              itemdesc   VARCHAR(255), 
-                              comments   VARCHAR(255), 
-                              purchprice DECIMAL(10,2), 
-                              purchdate  DATETIME, 
-                              sellprice  DECIMAL(10,2), 
-                              selldate   DATETIME, 
-                              quantity   INT, 
-                              PRIMARY KEY (invid), 
-                              INDEX invid (invid), 
-                              INDEX projectid (projectid) 
-                 );
+CREATE TABLE workcompleted 
+( 
+    workid          INT           NOT NULL   auto_increment, 
+    projectid       INT           NOT NULL, 
+    workname        VARCHAR(60)   NOT NULL, 
+    dateperformed   DATETIME      NOT NULL,
+    workdesc        VARCHAR(255)  NOT NULL, 
+    workcomments    VARCHAR(255), 
+    PRIMARY KEY (workid),
+    INDEX projectid (projectid)
+    /*FOREIGN KEY (projectid) REFERENCES projects (projectid)*/ 
+);
                  
-                 CREATE TABLE projects 
-                 ( 
-                              projectid       INT auto_increment, 
-                              projectname     VARCHAR(60), 
-                              projectdesc     VARCHAR(255), 
-                              projectcomments VARCHAR(255), 
-                              purchprice      DECIMAL(10,2), 
-                              purchdate       DATETIME, 
-                              sellprice       DECIMAL(10,2), 
-                              selldate        DATETIME, 
-                              PRIMARY KEY (projectid), 
-                              INDEX projectid (projectid)
-                 );
-                 
-                 CREATE TABLE workcompleted 
-                 ( 
-                              workid        INT auto_increment, 
-                              projectid     INT, 
-                              workname      VARCHAR(60), 
-                              workdesc      VARCHAR(255), 
-                              workcomments  VARCHAR(255), 
-                              dateperformed DATETIME, 
-                              PRIMARY KEY (workid) 
-                 );
-                 
-                 CREATE TABLE files 
-                 ( 
-                              fileid    INT auto_increment, 
-                              projectid INT, 
-                              filename  VARCHAR(60), 
-                              filetype  VARCHAR(60), 
-                              dateadded DATETIME, 
-                              PRIMARY KEY (fileid) 
-                 ); 
+CREATE TABLE files 
+( 
+    fileid          INT            NOT NULL   auto_increment, 
+    projectid       INT            NOT NULL, 
+    filename        VARCHAR(60)    NOT NULL, 
+    filetype        VARCHAR(60)    NOT NULL, 
+    dateadded       DATETIME       NOT NULL, 
+    PRIMARY KEY (fileid),
+    INDEX projectid (projectid)
+    /* FOREIGN KEY (projectid) REFERENCES projects (projectid)*/
+); 
+
+CREATE TABLE users
+(
+    userid          INT            NOT NULL     auto_increment,
+    username        VARCHAR(20)    NOT NULL,
+    password        VARCHAR(20)    NOT NULL,
+    PRIMARY KEY (userid),
+    UNIQUE INDEX (username),
+    UNIQUE INDEX (password) 
+);
+
      
 
 
