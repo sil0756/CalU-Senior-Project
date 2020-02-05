c4c94b09fb72eec1647083d5534a7eab7efce27f
Mitch Zink <47302583+mitch-zink@users.noreply.github.com>
2020-02-05 18:01:48 -0500

    Added pages and adjusted the nav bar

--- homepage.php
+++ homepage.php
@@ -8,12 +8,17 @@
 <ul>
   <li style="float:left"><a href="#">Bronco</a>
   <li><a class="active" href="homepage.php">Home</a></li>
+  <li><a href="aboutus.php">About Us</a></li>
+  <li><a href="purpose.php">Purpose</a></li>
+  <li><a href="faq.php">FAQ</a></li>
+  <li><a href="createUA.php">Create User Account</a></li>
+  <li><a href="login.php">Login</a></li>
+  <li><a href="logout.php">Logout</a></li>
   <li><a href="inventory.php">Inventory</a></li>
   <li><a href="phonebook.php">Phonebook</a></li>
   <li><a href="projects.php">Projects</a></li>
   <li><a href="files.php">Files</a></li>
   <li><a href="WorkCompleted.php">Work Completed</a></li>
 </ul>
-
 </body>
 </html>
