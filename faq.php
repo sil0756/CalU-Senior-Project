622cbbf859b1814f120f67dff3b73dcefa65218e
sil0756 <57736111+sil0756@users.noreply.github.com>
2020-02-07 14:24:26 -0500

    added code for wording

--- faq.php
+++ faq.php
@@ -25,6 +25,17 @@
 </ul>
 
 <h3> FAQ </h3>
+<?php 
+  echo "<h3>FAQ</h3>";
+  echo "<hr>";
+  echo "<h3><pre>1) Can I create more than one project?
+  Answer: Yes, you can create as many projects as you can manage.
+  2) Can I delete a project after I‘ve sold it?
+  Answer: Yes, you can delete a project at any time you no longer need it.
+  3) Can I use this tool to tracker other makes, models and years of project cars?
+  Answer: Yes, other data is populated in the tables for this purpose.
+ </h3></pre>";
+?>
 
 </body>
 </html>
