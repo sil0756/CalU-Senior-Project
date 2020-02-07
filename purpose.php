3cfca24bb4208187ea9b72655c233b2cd2fe1db4
sil0756 <57736111+sil0756@users.noreply.github.com>
2020-02-07 14:26:29 -0500

    update code for wording

--- purpose.php
+++ purpose.php
@@ -25,6 +25,15 @@
 </ul>
 
 <h3> Purpose Page </h3>
+<?php 
+echo "<h3>Purpose</h3>";
+echo "<hr>";
+echo "<h3><pre>This website is designed for the 1966 – 1977 Early Ford Bronco enthusiast to track vehicle 
+expenses & maintenance from the initial purchase to the bittersweet day when ownership changes hands. 
+It also provides a convenient location to store files, such as user manuals, warranties, receipts, invoices, 
+pictures & videos. Hover over “Start Here” and click on “Create User Account” to begin.
+</h3></pre>";
+?>
 
 </body>
 </html>
