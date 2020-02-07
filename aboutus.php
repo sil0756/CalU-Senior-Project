794f415a7128e56627235ce03f9b53d0f0387e66
sil0756 <57736111+sil0756@users.noreply.github.com>
2020-02-07 14:29:24 -0500

    coded wording for about us
    i will need 2 upload 2 jpg files to make them available for display

--- aboutus.php
+++ aboutus.php
@@ -23,6 +23,17 @@
   <li><a href="createnewproject.php">Create New Project</a></li>
   <li><a href="openproject.php">Open Project</a></li>
 </ul>
+  <?php 
+echo "<h3>About Us</h3>";
+echo "<hr>";
+echo ("<h3><pre>My passion for the Early Ford Bronco began as a teenager, when my hunting buddies picked me up in one and we set out on our deer hunting adventure. 
+Since that moment on, I started saving my pennies and searching for a Bronco to call my own. I found a 1972 rusty steed in a farmer’s field with a 
+small tree growing up through the engine block and turned it into this dream machine (pictured below) built for off road and street cruising!</pre>");
+echo "<img src=1972_Bronco.jpg width=300 height=200>";
+echo ("<pre>After enjoying my trusty steed for a few decades, I sold it and bought a 1974 Bronco in rough shape (pictured below). 
+Time to start tracking how much this newest project is costing me.</pre></h3>");
+echo "<img src=1974_Bronco.jpg width=300 height=200>";
+?>
 
 </body>
 </html>
